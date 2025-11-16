0bd9b2d75fe79aba606c694ed0db90614a6de13c
MauricioMVilcapoma <mauricio.m.vilcapoma@gmail.com>
2025-11-16 17:03:48 -0500

    Fix autorizhe buttom

--- src/main/java/pe/edu/upc/vitalia/vitalia_backend/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/pe/edu/upc/vitalia/vitalia_backend/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +15,13 @@ public class OpenApiConfiguration {
     @Bean
     public OpenAPI vitaliaOpenApi() {
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
                 .info(new Info()
                         .title("Vitalia API")
                         .description("""
